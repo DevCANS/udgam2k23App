e466e428f0a5539b632c6d8f53b15eb68f76e873
ansi11290 <ansi29110@gmail.com>
2023-03-04 20:20:57 +0530

    Initial

---
+++ udgam2k23/android/app/src/main/kotlin/com/example/udgam2k23/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udgam2k23
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ udgam2k23/lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromARGB(255, 226, 195, 233);
